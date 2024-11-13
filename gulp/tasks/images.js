589d7ad3e1052745c92c83152df3760e8dedb9ca
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-11-13 16:29:39 +0200

    Added fonts.scss generation

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -13,7 +13,7 @@ export const images = () => {
       )
     )
     .pipe(app.plugins.newer(app.path.build.images))
-    .pipe(webp()) // Convert images to WebP
+    .pipe(webp())
     .pipe(app.gulp.dest(app.path.build.images))
 
     .pipe(app.gulp.src(app.path.src.images, { encoding: false }))
